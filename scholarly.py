8a6829d6bec714b5afdb66a33c68b646ebcc68f8
lipi1994 <65544043+lipi1994@users.noreply.github.com>
2021-02-23 15:19:26 +0100

    Update scholarly.py

--- scholarly.py
+++ scholarly.py
@@ -149,39 +149,7 @@ class Publication(object):
 
     def fill(self):
         """Populate the Publication with information from its profile"""
-        if self.source == 'citations':
-            url_citations = _CITATIONPUB.format(self.id_citations)
-            soup = _get_soup(url_citations)
-            self.bib['title'] = soup.find('div', id='gsc_title').text
-            if soup.find('a', class_='gsc_title_link'):
-                self.bib['url'] = soup.find('a', class_='gsc_title_link')['href']
-            for item in soup.findAll('div', class_='gs_scl'):
-                key = item.find(class_='gsc_field').text
-                val = item.find(class_='gsc_value')
-                if key == 'Authors':
-                    self.bib['author'] = ' and '.join([i.strip() for i in val.text.split(',')])
-                elif key == 'Journal':
-                    self.bib['journal'] = val.text
-                elif key == 'Volume':
-                    self.bib['volume'] = val.text
-                elif key == 'Issue':
-                    self.bib['number'] = val.text
-                elif key == 'Pages':
-                    self.bib['pages'] = val.text
-                elif key == 'Publisher':
-                    self.bib['publisher'] = val.text
-                elif key == 'Publication date':
-                    self.bib['year'] = arrow.get(val.text).year
-                elif key == 'Description':
-                    if val.text[0:8].lower() == 'abstract':
-                        val = val.text[9:].strip()
-                    self.bib['abstract'] = val
-                elif key == 'Total citations':
-                    self.id_scholarcitedby = re.findall(_SCHOLARPUBRE, val.find('a')['href'])[0]
-            if soup.find('div', class_='gsc_title_ggi'):
-                self.bib['eprint'] = soup.find('div', class_='gsc_title_ggi').a['href']
-            self._filled = True
-        elif self.source == 'scholar':
+        if self.source == 'scholar':
             bibtex = _get_page(self.url_scholarbib)
             self.bib.update(bibtexparser.loads(bibtex).entries[0])
             self._filled = True
@@ -203,92 +171,10 @@ class Publication(object):
         return pprint.pformat(self.__dict__)
 
 
-class Author(object):
-    """Returns an object for a single author"""
-    def __init__(self, __data):
-        if isinstance(__data, str):
-            self.id = __data
-        else:
-            self.id = re.findall(_CITATIONAUTHRE, __data('a')[0]['href'])[0]
-            self.url_picture = __data('img')[0]['src']
-            self.name = __data.find('h3', class_='gsc_1usr_name').text
-            affiliation = __data.find('div', class_='gsc_1usr_aff')
-            if affiliation:
-                self.affiliation = affiliation.text
-            email = __data.find('div', class_='gsc_1usr_emlb')
-            if email:
-                self.email = email.text
-            self.interests = [i.text.strip() for i in __data.findAll('a', class_='gsc_co_int')]
-            citedby = __data.find('div', class_='gsc_1usr_cby')
-            if citedby:
-                self.citedby = int(citedby.text[9:])
-        self._filled = False
-
-    def fill(self):
-        """Populate the Author with information from their profile"""
-        url_citations = _CITATIONAUTH.format(self.id)
-        soup = _get_soup('{0}&pagesize={1}'.format(url_citations, _PAGESIZE))
-        self.name = soup.find('div', id='gsc_prf_in').text
-        self.affiliation = soup.find('div', class_='gsc_prf_il').text
-        self.interests = [i.text.strip() for i in soup.findAll('a', class_='gsc_prf_ila')]
-        self.url_picture = soup.find('img')['src']
-
-	#h-index, i10-index and h-index, i10-index in the last 5 years
-        index = soup.findAll('td', class_='gsc_rsb_std')
-        self.hindex = int(index[2].text)
-        self.hindex5y = int(index[3].text)
-        self.i10index = int(index[4].text)
-        self.i10index5y = int(index[5].text)
-        
-        self.publications = list()
-        pubstart = 0
-        while True:
-            for row in soup.findAll('tr', class_='gsc_a_tr'):
-                new_pub = Publication(row, 'citations')
-                self.publications.append(new_pub)
-            if 'disabled' not in soup.find('button', id='gsc_bpf_next').attrs:
-                pubstart += _PAGESIZE
-                soup = _get_soup('{0}&cstart={1}&pagesize={2}'.format(url_citations, pubstart, _PAGESIZE))
-            else:
-                break
-        self._filled = True
-        return self
-
-    def __str__(self):
-        return pprint.pformat(self.__dict__)
-
-
 def search_pubs_query(query):
     """Search by scholar query and return a generator of Publication objects"""
     soup = _get_soup(_PUBSEARCH.format(requests.utils.quote(query)))
     return _search_scholar_soup(soup)
-
-
-def search_author(name):
-    """Search by author name and return a generator of Author objects"""
-    soup = _get_soup(_AUTHSEARCH.format(requests.utils.quote(name)))
-    return _search_citation_soup(soup)
-
-
-def search_keyword(keyword):
-    """Search by keyword and return a generator of Author objects"""
-    soup = _get_soup(_KEYWORDSEARCH.format(requests.utils.quote(keyword)))
-    return _search_citation_soup(soup)
-
-
-def search_pubs_custom_url(url):
-    """Search by custom URL and return a generator of Publication objects
-    URL should be of the form '/scholar?q=...'"""
-    soup = _get_soup(url)
-    return _search_scholar_soup(soup)
-
-
-def search_author_custom_url(url):
-    """Search by custom URL and return a generator of Publication objects
-    URL should be of the form '/citation?q=...'"""
-    soup = _get_soup(url)
-    return _search_citation_soup(soup)
-
 
 if __name__ == "__main__":
     author = next(search_author('Steven A. Cholewiak')).fill()
